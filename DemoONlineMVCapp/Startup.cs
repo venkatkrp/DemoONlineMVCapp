586ffce2f60435c19ec86d581eb9daf6303bf18c
venkatkrp <venkatkrp@gmail.com>
2016-11-09 19:07:32 +0530

    Add project files.

---
+++ DemoONlineMVCapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoONlineMVCapp.Startup))]
+namespace DemoONlineMVCapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
